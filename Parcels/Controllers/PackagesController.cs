b83fdf68c6cf7cd2d94e16cdf2964dd987f6b8b9
Jonathan Cheng <jcheng461@gmail.com>
2023-07-03 23:02:49 +0900

    set up Parcels.Solution foundation files plus Package Constructor pass 1st test

---
+++ Parcels/Controllers/PackagesController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+using System.Collections.Generic;
+
+namespace Parcels.Controllers
+{
+  public class PackagesController : Controller
+  {
+    // [HttpGet("/inventory")]
+    // public ActionResult Index()
+    // {
+    //   List<Entry> allEntries = Entry.GetAll();
+    //   return View(allEntries);
+    // }
+
+    // [HttpGet("/inventory/newentry")]
+    // public ActionResult CreateForm()
+    // {
+    //   return View();
+    // }
+
+    // [HttpPost("/inventory")]
+    // public ActionResult Create(string vehicleType, int vehicleYear, string vehicleMake, string vehicleModel)
+    // {
+    //   Entry anEntry = new Entry(vehicleType, vehicleYear, vehicleMake, vehicleModel);
+    //   return RedirectToAction("Index");
+    // }
+
+  }
+}
